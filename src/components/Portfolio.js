bb72145a8e6c2dc386d9550e8511e5da6e50811e
kuiil7 <kuiil7ig11@gmail.com>
2021-03-01 11:36:46 -0800

    updated top margin in Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -66,7 +66,7 @@ function Portfolio () {
 
 
  return (
-    <div className="columns container is-flex is-justify-content-center	 is-flex-wrap-wrap p-4  mt-5">
+    <div className="columns container is-flex is-justify-content-center	 is-flex-wrap-wrap p-4  " style={{marginTop:"100px"}}>
     {data.map(function(data, i){
 
        return (
