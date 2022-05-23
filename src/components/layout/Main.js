d54a51dfb03e472c60476dafb519b29728ad72f5
kuiil7 <jgamerino@gmail.com>
2022-05-23 08:55:14 -0700

    test

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -17,21 +17,16 @@ Your browser does not support the video tag.
 
   <div className="hero-body">
     <div className="container has-text-centered">
-    <Link className="link " to="/aboutme">
-      <h3 className="heading is-size-1">JGAMWORKS</h3>
-      </Link>
+    <Link className="link" to="/aboutme"><h1 className="has-text-centered title is-size-1  has-text-white ">JGAMWORKS</h1>
+    </Link>
 <h2 className="has-text-centered subtitle is-size-6 is-italic has-text-white">
 Full Stack Development (MERN) + ASL Linguistics = Inclusive Web Design. </h2>
 
-<<<<<<< HEAD
 
 <nav className="level is-mobile has-text-white ">
-=======
-<nav className="level is-mobile ">
->>>>>>> e8ee6129d44ab4d0c5a0fed0c3bc9a54323b73f2
   <div className="level-item ">
     <div>
-    <Link className="link " to="/api">
+    <Link className="link has-text-white " to="/api">
     <i className="fa-solid fa-arrow-right-arrow-left fa-3x has-text-white"></i>
       <h3 className="heading ">API</h3>
       </Link>
@@ -40,7 +35,7 @@ Full Stack Development (MERN) + ASL Linguistics = Inclusive Web Design. </h2>
 
   <div className="level-item ">
     <div>
-    <Link className="link " to="/bootcamp">
+    <Link className="link has-text-white " to="/bootcamp">
     <i className="fa-solid fa-graduation-cap fa-3x has-text-white"></i>
       <h3 className="heading ">Bootcamp</h3>
       </Link>
@@ -49,7 +44,7 @@ Full Stack Development (MERN) + ASL Linguistics = Inclusive Web Design. </h2>
 
   <div className="level-item ">
     <div>
-    <Link className="link " to="/inclusion">
+    <Link className="link has-text-white " to="/inclusion">
     <img src={inc2} alt="inclusion sign in ASL"  />
 
       <h3 className="heading ">Inclusion</h3>
@@ -61,9 +56,9 @@ Full Stack Development (MERN) + ASL Linguistics = Inclusive Web Design. </h2>
    <Link className="link has-text-white" to="/static">
     <i className="fa-solid fa-display fa-3x has-text-white"></i>
       <h3 className="heading " >
-        <span className="link">Static</span>
+        <span>Static</span>
         <br/>
-        <span className="link">Websites</span>
+        <span>Websites</span>
       </h3>
       </Link>
     </div>
@@ -71,11 +66,11 @@ Full Stack Development (MERN) + ASL Linguistics = Inclusive Web Design. </h2>
   <div className="level-item ">
     <div>
    <Link className="link has-text-white" to="/Designs">
-   <i className="fa-solid fa-paintbrush fa-3x"></i>
+   <i className="fa-solid fa-paintbrush has-text-white fa-3x"></i>
    <h3 className="heading " >
-        <span className="link">Graphic</span>
+        <span>Graphic</span>
         <br/>
-        <span className="link">Design</span>
+        <span>Design</span>
       </h3>
    </Link>
     </div>
